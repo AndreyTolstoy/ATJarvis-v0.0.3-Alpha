eb1c83bfd91b443d23226b1638e9cbe4f732cc2f
AndreyTolstoy <161819069+AndreyTolstoy@users.noreply.github.com>
2024-09-12 12:33:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ class Receive_and_process:
          
        
        data = json_load("path.json")
-       #print(data)
        while True:
          rec_commands = listen()
          
@@ -32,13 +31,10 @@ class Receive_and_process:
                     print("Распознана команда: ", command.command_name)
 
                     if command.open_prg == "None":
-                        #print(command.command_name)
                         open_prog_list = {command.command_name : data[command.command_name]}
                         print(open_prog_list[command.command_name])
                         command.open_prg = open_prog_list[command.command_name]
                         
-                    #elif command.command_name == "search_in_youtube":
-                        #youtube()
 
    
                     
